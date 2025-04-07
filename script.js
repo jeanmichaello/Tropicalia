6061585fcd3e01e307608c1723c0a95ba13893c7
jeanmichaello <jean.oliveira22@escola.pr.gov.br>
2025-04-07 18:59:35 -0300

    c

--- script.js
+++ script.js
@@ -19,6 +19,11 @@ document.addEventListener('DOMContentLoaded', function () {
    botaoDeAcessibilidade.addEventListener('click', function(){
     botaoDeAcessibilidade.classList.toggle('rotacao-botao');
     opcoesDeAcessibilidade.classList.toggle('apresenta-lista');
+    const alternaContraste = document.getElementById('alterna-contraste')
+
+    alternaContraste.addEventListener('click', function () {
+        document.body.classList.toggle('alto-contraste')
+    })
 
    })
 
